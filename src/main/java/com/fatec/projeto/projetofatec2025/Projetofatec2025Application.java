b73d2d1026d0f0612f51a5dee7f306b95e801628
Oblytoid <cauan.nogueira@outlook.com.br>
2025-02-19 19:35:22 -0300

    Adicionando Spring ao projeto

---
+++ src/main/java/com/fatec/projeto/projetofatec2025/Projetofatec2025Application.java
@@ -0,0 +1,13 @@
+package com.fatec.projeto.projetofatec2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetofatec2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetofatec2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/projeto/projetofatec2025/Projetofatec2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.projeto.projetofatec2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetofatec2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
