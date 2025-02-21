0d6e975d1fdc7f404bc742e16790bd1b6e6f6987
Oblytoid <cauan.nogueira@outlook.com.br>
2025-02-20 22:43:25 -0300

    Update 2 20/02/2025

--- src/main/java/com/fatec/projeto/projetofatec2025/controller/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projetofatec2025/controller/ExercicioController.java
@@ -1,13 +1,14 @@
 package com.fatec.projeto.projetofatec2025.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
     
-    @GetMapping("")
-    public String HelloWorld(){
-        return "<h1"+" style ="+"color:red"+">Hello, World?</h1><h2>Oque é a vida?</h2>";
+    @GetMapping("{nome}")
+    public String HelloWorld(@PathVariable String nome){
+        return "<h1"+" style ="+"color:red"+">Hello, World?</h1><h2>"+nome+"</h2>";
     }
 }
