ab150042f1cdad9f02a5ea245de843a8d6eddc05
Oblytoid <cauan.nogueira@outlook.com.br>
2025-02-26 19:36:00 -0300

    salvar

--- src/main/java/com/fatec/projeto/projetofatec2025/controller/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projetofatec2025/controller/ExercicioController.java
@@ -3,6 +3,8 @@ package com.fatec.projeto.projetofatec2025.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ExercicioController {
@@ -11,4 +13,22 @@ public class ExercicioController {
     public String HelloWorld(@PathVariable String nome){
         return "<h1"+" style ="+"color:red"+">Hello, World?</h1><h2>"+nome+"</h2>";
     }
+
+    @GetMapping("age")
+    public String getMethodName(@RequestParam String age) {
+
+        int currentAge = Integer.parseInt(age);
+
+        if (currentAge > 60) {
+            return "Idoso";
+        }
+        if (currentAge > 19) {
+            return "Adulto";
+        }
+        if (currentAge > 12) {
+            return "Adolecente";
+        }
+        return "Criança";
+    }
+    
 }
