5ea4d33fcbf1d13a037b04ac73babd7327352624
Oblytoid <cauan.nogueria@outlook.com.br>
2025-04-23 22:09:07 -0300

    Update 23/04

---
+++ src/main/java/com/fatec/projeto/fatec2025/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.fatec.projeto.fatec2025.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
